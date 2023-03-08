ffe5db0415fee6649f5cbb2ef550508e41231989
Alexander Balashov <balashov187@yandex.ru>
2023-03-08 18:42:08 +0300

    Вторая задача

--- Program.cs
+++ Program.cs
@@ -4,10 +4,41 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
+// Console.Write("Введите певрое число: ");
+// int A = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите Второе число: ");
+// int B = int.Parse(Console.ReadLine()!);
+// if (A>B)Console.Write("Первое число большее второго " );
+// if (A<B)Console.Write("Первое число меньше второго " );
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
 Console.Write("Введите певрое число: ");
 int A = int.Parse(Console.ReadLine()!);
-Console.Write("Введите Второе число: ");
+Console.Write("Введите второе число: ");
 int B = int.Parse(Console.ReadLine()!);
-if (A>B)Console.Write("Первое число большее второго " );
-if (A<B)Console.Write("Первое число меньше второго " );
-    
+Console.Write("Введите третье число: ");
+int C = int.Parse(Console.ReadLine()!);
+if (A > B)
+{
+    if (A > C)
+    {
+        Console.WriteLine("Максимальное число: " + A);
+    }
+    else
+    {
+        Console.WriteLine("Максимальное число: " + C);
+    }
+}
+
+else if (B > C)
+{
+    Console.WriteLine("Максимальное число: " + B);
+}
+else
+{
+    Console.WriteLine("Максимальное число: " + C);
+}
